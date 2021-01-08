457c90307dd3411a1c45eb908dbace2006e48b9b
Serge Ilyashenko <serj.ilyashenko@gmail.com>
2021-01-08 18:53:27 +0300

    feat(RateFetch): nbrb api integration;

--- src/Hooks/RateFetch.js
+++ src/Hooks/RateFetch.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import queryString from "query-string";
 
 const url = `https://www.nbrb.by/API/ExRates/Rates/Dynamics/145?startDate=Sat%2C+11+Jan+2020+21%3A00%3A00+GMT&endDate=Thu%2C+07+Jan+2021+21%3A00%3A00+GMT`; // USD code: 145
 
