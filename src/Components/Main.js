f02990792c3274027e4dc908584972b75abc6a94
Serge Ilyashenko <serj.ilyashenko@gmail.com>
2021-01-08 18:49:09 +0300

    feat(RateFetch): nbrb api integration;

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,30 +1,63 @@
 import { useRef, useEffect } from "react";
 import * as d3 from "d3";
+import { useBYN } from "../Hooks/RateFetch";
 
 export default function Main() {
   const d3Ref = useRef(null);
+  const data = useBYN();
 
   useEffect(() => {
-    console.log(">> D3 useEffect");
-    const size = 500;
-    const svg = d3
-      .select(d3Ref.current)
-      .attr("width", size)
-      .attr("height", size);
+    console.log(">> D3 useEffect", data);
+
+    if (!data) {
+      return null;
+    }
+
+    const height = 800;
+    const width = 600;
+    const margin = { top: 0, right: 40, bottom: 0, left: 40 };
+
+    const svg = d3.select(d3Ref.current).attr("viewBox", [0, 0, width, height]);
     svg.selectAll("*").remove();
 
-    const rectWidth = 50;
+    const x = d3
+      .scaleTime()
+      .domain(d3.extent(data, (d) => d.date))
+      .range([margin.left, width - margin.right]);
+
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.value)])
+      .nice()
+      .range([height - margin.bottom, margin.top]);
+
+    // const radius = 2;
+    // svg
+    //   .selectAll("circle")
+    //   .data(data)
+    //   .enter()
+    //   .append("circle")
+    //   .attr("cx", (d) => x(d.date))
+    //   .attr("cy", (d) => y(d.value))
+    //   .attr("r", radius)
+    //   .attr("fill", "teal");
+
+    const line = d3
+      .line()
+      .defined((d) => !isNaN(d.value))
+      .x((d) => x(d.date))
+      .y((d) => y(d.value));
+
     svg
-      .selectAll("rect")
-      .data([100, 200, 300, 400, 500])
-      .enter()
-      .append("rect")
-      .attr("x", (d, i) => 5 + i * (rectWidth + 5))
-      .attr("y", (d) => size - d)
-      .attr("width", rectWidth)
-      .attr("height", (d) => d)
-      .attr("fill", "teal");
-  }, []);
+      .append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("d", line);
+  }, [data]);
 
   return (
     <div>
---
+++ src/Hooks/RateFetch.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import queryString from "query-string";
+
+const url = `https://www.nbrb.by/API/ExRates/Rates/Dynamics/145?startDate=Sat%2C+11+Jan+2020+21%3A00%3A00+GMT&endDate=Thu%2C+07+Jan+2021+21%3A00%3A00+GMT`; // USD code: 145
+
+export function useBYN() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    (async function () {
+      const response = await axios.get(url);
+
+      setData(
+        response?.data.map((it) => ({
+          date: new Date(it.Date),
+          value: it.Cur_OfficialRate,
+        }))
+      );
+    })();
+  }, []);
+
+  return data;
+}
